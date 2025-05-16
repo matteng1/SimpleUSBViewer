059d4b5375f344819de88a9f9cbe37fb7860179d
matteng1 <31434228+matteng1@users.noreply.github.com>
2025-05-16 15:24:27 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.libausbc)
+    implementation("com.github.jiangdongguo.AndroidUSBCamera:libausbc:3.3.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-}
